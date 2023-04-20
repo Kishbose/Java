042a2540dc669a811a3682899ba90d2d8a656b27
Kishbose <kaameshbose2002@gmail.com>
2023-04-20 19:54:29 +0530

    first commi

---
+++ src/demo/Day.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Day {
+
+	
+		public static void main(String[] args) {
+			String s = "My Name is Bose";
+			String st = " ";
+		for(int i=s.length()-1;i>-1;i--) {
+			st=st+s.charAt(i);
+		}
+		System.out.println("Reverse Type : "+st);
+		}
+
+	}
+
+
+
+
+
